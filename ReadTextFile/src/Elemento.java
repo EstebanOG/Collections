6f8186df6d6e893e3c5bcf83afb61603b790b69a
EstebanOG <juanesteban199911@gmail.com>
2021-04-05 14:58:08 -0500

    Se limpia codigo

--- ReadTextFile/src/Elemento.java
+++ ReadTextFile/src/Elemento.java
@@ -1,76 +1,37 @@
-
-import java.util.ArrayList;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Usuario
- */
 public class Elemento {
 
     private int padre, clave;
-    private ArrayList<Elemento> hijos;
-
-    public Elemento(int clave , int limInfHijos, int limSupHijos) {
-        this.hijos = new ArrayList();
+    
+    /**
+     * @param clave: Clave del nodo
+     * En caso de ser 0, se asiga por defecto -1 a su padre, ya que 0 es el nodo
+     * padre o raiz.
+     */
+    public Elemento(int clave) {
         this.clave = clave;
-        
-        if(clave==0){
+
+        if (clave == 0) {
             this.padre = -1;
         }
-        
-        if(limInfHijos != 0 && limSupHijos != 0){
-            for(int i = 0; i < (limSupHijos - limInfHijos) + 1; i++ ){
-            hijos.add(new Elemento(limInfHijos+i,0,0));
-            }
-        }
-        
-        
+
     }
     
-    public Elemento(int clave , int limInfHijos, int limSupHijos, int padre) {
-        this.hijos = new ArrayList();
+    /**
+     * 
+     * @param clave: Clave del nodo
+     * @param padre: Clave del nodo padre 
+     */
+    public Elemento(int clave, int padre) {
         this.clave = clave;
         this.padre = padre;
-        if(clave==0){
-            this.padre = -1;
-        }
-        
-        if(limInfHijos != 0 && limSupHijos != 0){
-            for(int i = 0; i < (limSupHijos - limInfHijos) + 1; i++ ){
-            hijos.add(new Elemento(limInfHijos+i,0,0));
-            }
-        }
-        
-        
     }
 
     public int getClave() {
         return clave;
     }
 
-    public void setClave(int clave) {
-        this.clave = clave;
-    }
-
     public int getPadre() {
         return padre;
-    }
-
-    public void setPadre(int padre) {
-        this.padre = padre;
-    }
-
-    public ArrayList<Elemento> getHijos() {
-        return hijos;
-    }
-
-    public void setHijos(ArrayList<Elemento> hijos) {
-        this.hijos = hijos;
     }
 
 }
--- ReadTextFile/src/InterfazApp.java
+++ ReadTextFile/src/InterfazApp.java
@@ -1,4 +1,3 @@
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,16 +17,34 @@ public class InterfazApp {
             while (input.hasNextLine()) {
                 line = input.nextLine();
                 vec = line.split(" ");
+                /**
+                 * vec[0]: Clave
+                 * vec[1]: Primer hijo
+                 * vec[2]: Ultimo hijo
+                 */
                 System.out.println(vec[0] + " " + vec[1] + " " + vec[2]);
-
+                
+                /**
+                 * Se evalua si la clave es 0, en caso de ser así, se crea una
+                 * instancia de la clase Elemento y se agrega a la colección en la primera posición (0),
+                 * además se le pasan las claves de los hijos.
+                 */
                 if (Integer.parseInt(vec[0]) == 0) {
                     coleccion.add(Integer.parseInt(vec[0]),
-                            new Elemento(Integer.parseInt(vec[0]), Integer.parseInt(vec[1]), Integer.parseInt(vec[2])));
+                            new Elemento(Integer.parseInt(vec[0])));
                 }
-
+                
+                /**
+                 * Se agregan a la colección, instancias de la clase Elemento, pero estas
+                 * además de recibir la clave y los hijos, reciben también el padre, que
+                 * en este caso es vec[0].
+                 * 
+                 * Estos elementos se agregan a la colección, en la posición correspondiente
+                 * a su clave, es por eso que mediante un ciclo se recorre el número de hijos.
+                 */
                 for (int i = 0; i < (Integer.parseInt(vec[2]) - Integer.parseInt(vec[1])) + 1; i++) {
                     coleccion.add(Integer.parseInt(vec[1]) + i,
-                            new Elemento(Integer.parseInt(vec[1]) + i, 0, 0, Integer.parseInt(vec[0])));
+                            new Elemento(Integer.parseInt(vec[1]) + i, Integer.parseInt(vec[0])));
 
                 }
             }
@@ -35,18 +52,29 @@ public class InterfazApp {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+        /**
+         * Leemos el nodo del cual queremos averiguar sus ancestros
+         */
         Scanner in = new Scanner(System.in);
         System.out.println("Ingrese el número del nodo: ");
         int a = in.nextInt();
 
         int act = coleccion.get(a).getClave();
+        
         List<Integer> solucion = new ArrayList<>();
-
+        /**
+         * Por medio del metodo getPadre(), obtenemos los ancestros hasta que
+         * este sea 0 y los vamos almacedando en la lista solucion.
+         */
         do {
             solucion.add(coleccion.get(act).getPadre());
             act = coleccion.get(act).getPadre();
 
         } while (act != 0);
+        
+        /**
+         * Imprimimos la lista de ancestros
+         */
         System.out.println(solucion);
     }
 }
